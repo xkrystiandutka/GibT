51745224ace62d78e85fd51e0e790f831d09cfef
Kingeh <artur.kasprzyk17@outlook.com>
2023-06-04 14:31:21 +0200

    Add theme for the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:gibt_app/theme/app-theme.dart';
 import 'package:gibt_app/utils/rive-utils.dart';
 import 'package:gibt_app/widgets/entry-point.dart';
 import 'package:rive/rive.dart';
@@ -16,26 +17,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const primaryColor = Color(0xFF7553F6);
-    final primarySwatch = MaterialColor(primaryColor.value, const {
-      50: primaryColor,
-      100: primaryColor,
-      200: primaryColor,
-      300: primaryColor,
-      400: primaryColor,
-      500: primaryColor,
-      600: primaryColor,
-      700: primaryColor,
-      800: primaryColor,
-      900: primaryColor,
-    });
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Countdown app',
-      theme: ThemeData(
-        primarySwatch: primarySwatch,
-      ),
+      theme: AppTheme.define(),
       home: const EntryPoint(),
     );
   }
---
+++ lib/theme/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  final lime = const Color(0xFFD4F653);
+  final primaryColor = const Color(0xFF7553F6);
+
+  const AppColors();
+}
